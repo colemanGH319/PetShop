8a442e41c1d97fd48785df0cd762f14695eaf094
Matt Coleman <coleman.matt319@gmail.com>
2019-03-26 18:13:05 -0400

    Initial commit

---
+++ src/components/Product.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export default class Product extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello from product</h3>
+      </div>
+    )
+  }
+}
